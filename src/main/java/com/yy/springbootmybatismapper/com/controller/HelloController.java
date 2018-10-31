333be92a3a1dd6663741aeffde3906f7a3c06498
yuanyuan.jing <sdf>
2018-10-31 17:53:38 +0800

    把pageSize做成参数，进行测试

--- src/main/java/com/yy/springbootmybatismapper/com/controller/HelloController.java
+++ src/main/java/com/yy/springbootmybatismapper/com/controller/HelloController.java
@@ -6,6 +6,7 @@ import com.yy.springbootmybatismapper.com.dao.UserInfoDao;
 import com.yy.springbootmybatismapper.com.entity.Country;
 import com.yy.springbootmybatismapper.com.entity.UserInfo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -36,10 +37,10 @@ public class HelloController {
      * 使用分页插件com.github.pagehelper.PageHelper进行，分页查询
      * @return
      */
-    @RequestMapping("/user/page")
-    public List<UserInfo> pageList(){
+    @RequestMapping("/user/{page}")
+    public List<UserInfo> pageList(@PathVariable("page") Integer page){
         //设置分页信息，分别是当前页数和每页显示的总记录数【记住：必须在mapper接口中的方法执行之前设置该分页信息】
-        PageHelper.startPage(1, 2);
+        PageHelper.startPage(page, 3);
         return userInfoDao.selectAll();
     }
 
